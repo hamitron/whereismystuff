0ef35cb16b1d0c02ceafb26edba567a8d8b667f9
hamitron <hqkarl@gmail.com>
2014-10-20 09:40:12 -0700

    sessions bug fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,6 @@
 class SessionsController < ApplicationController
 
 
-
-
 def new
 	@user = User.new
 	@is_login = true
@@ -17,16 +15,17 @@ def create
 		session[:user_id] = u.id.to_s
 		redirect_to root_path
 	else
-		redirect_to new_session_path
+		flash[:notice] = "No Success"
+		render 'new_session'
 	end
 end
 
 def destroy
 	reset_session
-	redirect_to root_path
+	redirect_to welcome_path
 end
 
-end
+
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+
+def index
+end
+
+def new
+	@user = User.new
+end
+
+def create
+	@user = User.new
+	if @user.save(user_params)
+		flash[:notice] = "Success"
+		redirect_to new_session_path
+
+	else
+		flash[:notice] = "No Success"
+		render 'new'
+	end
+
+end
+
+private
+def user_params
+	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
+
 end
